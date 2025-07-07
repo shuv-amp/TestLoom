d00fa4b5d531a12d23373a5d6b89a465c3d20e39
AshokMoktanTamang <tmgashok612@gmail.com>
2025-07-07 06:46:28 +0545

    Adding some features in SignUp and Login

--- app/pages/signup.vue
+++ app/pages/signup.vue
@@ -10,18 +10,22 @@
         <form @submit.prevent="handleSignup">
           <div class="mb-4">
             <label for="fullName" class="block text-sm font-medium text-gray-700 mb-1">Full Name</label>
-            <input type="text" id="fullName" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Niraj Ram" required>
+            <input v-model="text" id="fullName" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Niraj Ram" required>
           </div>
           <div class="mb-4">
             <label for="email" class="block text-sm font-medium text-gray-700 mb-1">Email Address</label>
-            <input type="email" id="email" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="you@example.com" required>
+            <input v-model="email" id="email" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="you@student.ku.edu.np" required>
           </div>
           <div class="mb-6">
             <label for="password" class="block text-sm font-medium text-gray-700 mb-1">Password</label>
-            <input type="password" id="password" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="••••••••••" required>
+            <input v-model="password" id="password" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="••••••••••" required>
+          </div>
+          <div class="mb-6">
+            <label for="cpassword" class="block text-sm font-medium text-gray-700 mb-1">Confirm Password</label>
+            <input v-model="cpassword" id="cpassword" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="••••••••••" required>
           </div>
           <div>
-            <button type="submit" class="w-full bg-blue-600 text-white py-2.5 rounded-md font-semibold hover:bg-blue-700 transition">
+            <button type="submit" @click="handleSubmit"  class="w-full bg-blue-600 text-white py-2.5 rounded-md font-semibold hover:bg-blue-700 transition">
               Create Account
             </button>
           </div>
@@ -35,3 +39,26 @@
     </div>
   </div>
 </template>
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const email = ref('')
+const password = ref('')
+const cpassword =ref('')
+
+const router = useRouter()
+
+function handleSubmit() {
+  if(password.value!==cpassword.value){
+    alert('Password didnot Matched')
+    return
+  }
+  if (!email.value.endsWith('@student.ku.edu.np')) {
+    alert('Please use a @student.ku.edu.np email address')
+    return
+  }
+  console.log('Login attempt with:', email.value)
+  router.push('/dashboard')
+}
+</script>
